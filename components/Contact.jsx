a3525dc5662fb9f845109aaf0e1026559ffb7207
Trevor Mathiak <tmathiak@gmail.com>
2022-11-21 23:02:37 -0500

    integrated sendgrid with contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -5,9 +5,101 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export const Contact = () => {
   const router = useRouter();
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [errors, setErrors] = useState({});
+  const [buttonText, setButtonText] = useState("Send Message");
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showFailureMessage, setShowFailureMessage] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {};
+    Array.from(e.currentTarget.elements).forEach((field) => {
+      if (!field.name) return;
+      formData[field.name] = field.value;
+    });
+    fetch("/api/sendgrid", {
+      method: "post",
+      body: JSON.stringify(formData),
+    });
+    console.log(formData);
+  };
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+  //   let isValidForm = handleValidation();
+
+  //   if (isValidForm) {
+  //     setButtonText("Sending...");
+  //     const res = await fetch("/api/sendgrid", {
+  //       body: JSON.stringify({
+  //         email: email,
+  //         name: name,
+  //         number: number,
+  //         subject: subject,
+  //         message: message,
+  //       }),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       method: "POST",
+  //     });
+
+  //     const { error } = await res.json();
+  //     if (error) {
+  //       console.log(error);
+  //       setShowSuccessMessage(false);
+  //       setShowFailureMessage(true);
+  //       setButtonText("Send Message");
+  //       setName("");
+  //       setNumber("");
+  //       setEmail("");
+  //       setSubject("");
+  //       setMessage("");
+  //       return;
+  //     }
+  //     setShowSuccessMessage(true);
+  //     setShowFailureMessage(false);
+  //     setButtonText("Send Message");
+  //     setName("");
+  //     setNumber("");
+  //     setEmail("");
+  //     setSubject("");
+  //     setMessage("");
+  //   }
+  //   console.log(name, email, number, subject, message);
+  // };
+
+  // const handleValidation = () => {
+  //   let tempErrors = {};
+  //   let isValid = true;
+
+  //   if (name.length <= 0) {
+  //     tempErrors["name"] = true;
+  //     isValid = false;
+  //   }
+  //   if (email.length <= 0) {
+  //     tempErrors["email"] = true;
+  //     isValid = false;
+  //   }
+  //   if (message.length <= 0) {
+  //     tempErrors["message"] = true;
+  //     isValid = false;
+  //   }
+
+  //   setErrors({ ...tempErrors });
+  //   console.log("errors", errors);
+  //   return isValid;
+  // };
 
   return (
     <div id="contact" className="w-full lg:h-screen p-2">
@@ -68,49 +160,105 @@ export const Contact = () => {
           </div>
           <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
             <div className="p-4">
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="grid md:grid-cols-2 gap-4 w-full py-2">
                   <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">Name</label>
+                    <label htmlFor="name" className="uppercase text-sm py-2">
+                      Name
+                    </label>
                     <input
                       className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="text"
+                      name="name"
+                      value={name}
+                      onChange={(e) => {
+                        setName(e.target.value);
+                      }}
                     />
+                    {errors?.name && (
+                      <p className="text-red-500">Please provide your name</p>
+                    )}
                   </div>
                   <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">
+                    <label htmlFor="number" className="uppercase text-sm py-2">
                       Phone Number
                     </label>
                     <input
                       className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="text"
+                      name="number"
+                      value={number}
+                      onChange={(e) => {
+                        setNumber(e.target.value);
+                      }}
                     />
                   </div>
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Email</label>
+                  <label htmlFor="email" className="uppercase text-sm py-2">
+                    Email
+                  </label>
                   <input
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="Email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
                   />
+                  {errors?.email && (
+                    <p className="text-red-500">
+                      Please provide your Email address
+                    </p>
+                  )}
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Subject</label>
+                  <label htmlFor="subject" className="uppercase text-sm py-2">
+                    Subject
+                  </label>
                   <input
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="text"
+                    name="subject"
+                    value={subject}
+                    onChange={(e) => {
+                      setSubject(e.target.value);
+                    }}
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Message</label>
+                  <label htmlFor="message" className="uppercase text-sm py-2">
+                    Message
+                  </label>
                   <textarea
                     className="border-2 rounded-lg p-3 border-gray-300"
                     rows="10"
+                    name="message"
+                    value={message}
+                    onChange={(e) => {
+                      setMessage(e.target.value);
+                    }}
                   ></textarea>
+                  {errors?.message && (
+                    <p className="text-red-500">Please provide a message</p>
+                  )}
                 </div>
                 <button className="w-full p-4 text-gray-100 mt-4">
-                  Send Message
+                  {buttonText}
                 </button>
+                <div>
+                  {showSuccessMessage && (
+                    <p className="text-green-500 font-semibold text-sm my-2">
+                      Thank You! Your Message has been delivered
+                    </p>
+                  )}
+                  {showFailureMessage && (
+                    <p className="text-red-500">
+                      Oops! Something went wrong, please try again
+                    </p>
+                  )}
+                </div>
               </form>
             </div>
           </div>
---
+++ pages/api/sendgrid.js
@@ -0,0 +1,27 @@
+const sgMail = require("@sendgrid/mail");
+console.log(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default (req, res) => {
+  const body = JSON.parse(req.body);
+
+  const message = `
+    Name: ${body.name}\r\n
+    Number: ${body.number}\r\n
+    Email: ${body.email}\r\n
+    Subject: ${body.subject}\r\n
+    Message: ${body.message}
+  `;
+
+  const data = {
+    to: "tmathiak@gmail.com",
+    from: "trevor@trevormathiak.dev",
+    subject: "New Portfolio Message!",
+    text: message,
+    html: message.replace(/\r\n/g, "<br>"),
+  };
+
+  sgMail.send(data);
+
+  res.status(200).json({ status: "OK" });
+};
