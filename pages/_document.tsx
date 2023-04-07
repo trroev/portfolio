6f73ded9ccf7cf9c28a4231ef79ab8f5ea5d6196
Trevor Mathiak <tmathiak@gmail.com>
2023-04-06 22:11:15 -0400

    updated GA gtag function and updated GA script function in document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,6 +24,7 @@ export default class MyDocument extends Document {
                   __html: `
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
                   gtag('config', '${GA_MEASUREMENT_ID}', {
                     page_path: window.location.pathname,
                   });
--- utils/gtag.ts
+++ utils/gtag.ts
@@ -23,6 +23,6 @@ export const event = ({
   window.gtag("event", action, {
     event_category: category,
     event_label: label,
-    value,
+    value: value,
   });
 };
