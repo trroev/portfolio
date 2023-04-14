3a7096fda63900ad783ff7a0ba987497e43d3762
Trevor Mathiak <tmathiak@gmail.com>
2023-04-13 21:48:34 -0400

    refactored Navbar to map over array of links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,19 @@ import { useState, useEffect } from "react";
 import { Icons } from "./Icons";
 import { useRouter } from "next/router";
 
+interface LinkType {
+  text: string;
+  href: string;
+}
+
+const links: LinkType[] = [
+  { text: "Home", href: "/" },
+  { text: "About", href: "/#about" },
+  { text: "Skills", href: "/#skills" },
+  { text: "Projects", href: "/#projects" },
+  { text: "Contact", href: "/#contact" },
+];
+
 export const Navbar = () => {
   const [nav, setNav] = useState<boolean>(false);
   const [navBackground, setNavBackground] =
@@ -36,23 +49,18 @@ export const Navbar = () => {
           <div className="w-12 h-12 rounded-full bg-gradient-to-br from-yellow-200 to-red-600 hover:scale-110 ease-in duration-300 cursor-pointer" />
         </Link>
         <div>
-          <ul className="hidden md:flex">
-            <li className="ml-10 text-sm uppercase hover:border-b">
-              <Link href="/">Home</Link>
-            </li>
-            <li className="ml-10 text-sm uppercase hover:border-b">
-              <Link href="/#about">About</Link>
-            </li>
-            <li className="ml-10 text-sm uppercase hover:border-b">
-              <Link href="/#skills">Skills</Link>
-            </li>
-            <li className="ml-10 text-sm uppercase hover:border-b">
-              <Link href="/#projects">Projects</Link>
-            </li>
-            <li className="ml-10 text-sm uppercase hover:border-b">
-              <Link href="/#contact">Contact</Link>
-            </li>
-          </ul>
+          <nav>
+            <ul className="hidden gap-6 md:flex">
+              {links?.map((link, index) => (
+                <li
+                  key={index}
+                  className="text-sm uppercase hover:text-red-400 hover:ease-in duration-300"
+                >
+                  <Link href={link.href}>{link.text}</Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
           <div
             onClick={handleNav}
             className="px-4 md:hidden cursor-pointer hover:text-red-400 hover:scale-110 ease-in duration-300"
@@ -77,7 +85,12 @@ export const Navbar = () => {
         >
           <div>
             <div className="flex w-full items-center justify-between">
-              <div className="w-10 h-10 rounded-full bg-gradient-to-br from-yellow-200 to-red-600 hover:scale-110 ease-in duration-300" />
+              <Link href="/" aria-label="Link to home page">
+                <div
+                  className="w-10 h-10 rounded-full bg-gradient-to-br from-yellow-200 to-red-600 hover:scale-110 ease-in duration-300"
+                  onClick={() => setNav(false)}
+                />
+              </Link>
               <div
                 onClick={handleNav}
                 className="p-3 cursor-pointer hover:text-red-400 hover:scale-110 ease-in duration-300"
@@ -92,38 +105,19 @@ export const Navbar = () => {
             </div>
           </div>
           <div className="py-4 flex flex-col">
-            <ul className="uppercase">
-              <li
-                onClick={() => setNav(false)}
-                className="py-4 text-sm hover:text-red-400 hover:ease-in duration-300"
-              >
-                <Link href="/">Home</Link>
-              </li>
-              <li
-                onClick={() => setNav(false)}
-                className="py-4 text-sm hover:text-red-400 hover:ease-in duration-300"
-              >
-                <Link href="/#about">About</Link>
-              </li>
-              <li
-                onClick={() => setNav(false)}
-                className="py-4 text-sm hover:text-red-400 hover:ease-in duration-300"
-              >
-                <Link href="/#skills">Skills</Link>
-              </li>
-              <li
-                onClick={() => setNav(false)}
-                className="py-4 text-sm hover:text-red-400 hover:ease-in duration-300"
-              >
-                <Link href="/#projects">Projects</Link>
-              </li>
-              <li
-                onClick={() => setNav(false)}
-                className="py-4 text-sm hover:text-red-400 hover:ease-in duration-300"
-              >
-                <Link href="/#contact">Contact</Link>
-              </li>
-            </ul>
+            <nav>
+              <ul className="uppercase">
+                {links?.map((link, index) => (
+                  <li
+                    key={index}
+                    className="py-4 text-sm hover:text-red-400 hover:ease-in duration-300"
+                    onClick={() => setNav(false)}
+                  >
+                    <Link href={link.href}>{link.text}</Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
             <div className="border-t my-4 pt-4">
               <p className="uppercase tracking-widest">
                 Let&#39;s Connect
