459b261e0ac3ff5740dcfa917e63cca6aa1360ad
Trevor Mathiak <tmathiak@gmail.com>
2023-04-17 17:38:32 -0400

    deleted headshot image and updated meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,11 +35,38 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <meta
           name="description"
           content="Portfolio website of full stack developer Trevor Mathiak"
+          key="desc"
         />
         <meta
           name="keywords"
-          content="full stack, web, developer, trevor, mathiak"
+          content="full stack, web, developer, trevor, mathiak, Next.js, React, TailwindCSS"
         />
+        <meta name="author" content="Trevor Mathiak" />
+        <meta name="creator" content="Trevor Mathiak" />
+        <meta
+          property="og:title"
+          content="Trevor Mathiak | Full Stack Developer"
+        />
+        <meta
+          property="og:description"
+          content="Portfolio website of full stack developer Trevor Mathiak"
+        />
+        <meta property="og:url" content="https://trevormathiak.dev" />
+        <meta
+          property="og:site_name"
+          content="Trevor Mathiak | Full Stack Developer"
+        />
+        <meta
+          property="og:image"
+          content="https://trevormathiak.dev/screenshot.jpg"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta
+          property="og:image:alt"
+          content="Trevor Mathiak | Full Stack Developer"
+        />
+        <meta property="og:type" content="website" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
