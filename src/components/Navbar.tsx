e6432e4d46f461fea1c5e3464cb837fb95cd2890
Trevor Mathiak <tmathiak@gmail.com>
2023-06-08 14:37:55 -0400

    Updated Navbar and globals.css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,24 +21,24 @@ const Navbar: FC<NavbarProps> = ({ items }) => {
 
   return (
     <header className="container sticky top-0 z-40 bg-slate-950/80 backdrop-blur-sm">
-      <div className="flex h-20 items-center justify-between border-b border-b-slate-200 py-4">
+      <div className="flex h-20 items-center justify-between border-b border-b-slate-200 py-6">
         <Link
           href="/"
           aria-label="Link to home page"
-          className="items-center space-x-2 sm:flex hover:scale-105 duration-300"
+          className="items-center space-x-2 md:flex"
         >
-          <div className="w-12 h-12 rounded-full bg-gradient-to-br from-yellow-200 to-red-600 hover:scale-110 ease-in duration-300 cursor-pointer" />
-          <span className="hidden sm:inline-block">
+          <div className="w-12 h-12 rounded-full bg-gradient-to-br from-yellow-200 to-red-600 cursor-pointer" />
+          <span className="hidden font-bold md:inline-block">
             {siteConfig.title}
           </span>
         </Link>
         <div className="flex gap-6 md:gap-10">
-          <nav className="hidden gap-6 md:flex">
+          <nav className="hidden gap-6 md:flex ml-auto">
             {items?.map((item, index) => (
               <Link
                 key={index}
                 href={item.href}
-                className="flex items-center text-lg font-semibold sm:text-sm hover:underline"
+                className="flex items-center text-lg text-slate-50/70 font-medium transition-colors hover:text-slate-100 sm:text-sm"
               >
                 {item.title}
               </Link>
@@ -46,7 +46,7 @@ const Navbar: FC<NavbarProps> = ({ items }) => {
           </nav>
         </div>
         <button
-          className="flex items-center space-x-2 md:hidden ml-auto hover:scale-110 duration-300"
+          className="flex items-center space-x-2 md:hidden hover:scale-110 duration-300"
           onClick={handleNav}
         >
           {nav ? <Icons.close /> : <Icons.menu />}
