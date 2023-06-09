e8ecc54a6d5f5849e4434e7311d2dbb09c43cfb7
Trevor Mathiak <tmathiak@gmail.com>
2023-06-09 13:58:02 -0400

    Added onClick to logo link

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -2,7 +2,6 @@ import { FC } from "react";
 import Link from "next/link";
 
 import { NavLink } from "@/types";
-import { Icons } from "./Icons";
 import { useLockBody } from "@/hooks/lock-body";
 import { siteConfig } from "@/config/site";
 
@@ -23,7 +22,11 @@ const MobileNav: FC<MobileNavProps> = ({ items, onClose }) => {
   return (
     <div className="fixed inset-0 top-16 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-6 pb-32 shadow-md animate-in slide-in-from-bottom-80 md:hidden">
       <div className="relative z-20 grid gap-6 rounded-md bg-slate-950 p-4 shadow-md">
-        <Link href="/" className="flex items-center space-x-4">
+        <Link
+          href="/"
+          className="flex items-center space-x-4"
+          onClick={handleLinkClick}
+        >
           <div className="w-10 h-10 rounded-full bg-gradient-to-br from-yellow-200 to-red-600 cursor-pointer" />
           <span className="font-medium">{siteConfig.title}</span>
         </Link>
