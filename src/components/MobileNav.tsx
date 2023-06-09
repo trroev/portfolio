535261309db3ec32603fa3fa48f78edf9f716223
Trevor Mathiak <tmathiak@gmail.com>
2023-06-09 16:36:12 -0400

    Added background opacity to mobile nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -20,7 +20,7 @@ const MobileNav: FC<MobileNavProps> = ({ items, onClose }) => {
   };
 
   return (
-    <div className="fixed inset-0 top-16 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-6 pb-32 shadow-md animate-in slide-in-from-bottom-80 md:hidden">
+    <div className="fixed inset-0 top-16 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-6 pb-32 shadow-md bg-slate-950/75 animate-in slide-in-from-bottom-80 md:hidden">
       <div className="relative z-20 grid gap-6 rounded-md bg-slate-950 p-4 shadow-md">
         <Link
           href="/"
