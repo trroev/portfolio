928aa525a267c4e0dd78a315a9d5e0c74d319f68
Trevor Mathiak <tmathiak@gmail.com>
2025-01-19 12:12:43 -0500

    chore: update next config

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+export default nextConfig
