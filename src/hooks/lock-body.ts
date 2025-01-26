4b00a853e41be33bf823a12a625a3f0f4e42e348
Trevor Mathiak <tmathiak@gmail.com>
2025-01-26 08:58:15 -0500

    fix: biome error

--- src/hooks/lock-body.ts
+++ src/hooks/lock-body.ts
@@ -3,12 +3,12 @@ import { useLayoutEffect } from 'react'
 export function useLockBody() {
   useLayoutEffect((): (() => void) => {
     const originalStyle: string = window.getComputedStyle(
-      document.body
+      document.body,
     ).overflow
     document.body.style.overflow = 'hidden'
 
     return () => {
-      document.body.style.overflow = originalStyle;
+      document.body.style.overflow = originalStyle
     }
   }, [])
 }
